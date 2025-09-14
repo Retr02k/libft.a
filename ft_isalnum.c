63feb02e9a45fb8c4b5a6928544ef57080e214ea
Retr02k <pedro210332@gmail.com>
2025-09-14 18:01:27 +0100

    ft_isalnum && moded libft.h

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,15 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psilva-p <psilva-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/14 17:42:16 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/09/14 17:47:23 by psilva-p         ###   ########.fr       */
+/*   Created: 2025/09/14 17:41:23 by psilva-p          #+#    #+#             */
+/*   Updated: 2025/09/14 18:50:09 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalnum(char c);
-int	ft_isalpha(char c);
-int	ft_isdigit(char c);
+#include "libft.h"
+#include <stdio.h>
+
+int	ft_isalnum(char c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	else
+		return (0);
+}
+
+int main()
+{
+	printf("%i\n", ft_isalnum('a'));
+	printf("%i\n", ft_isalnum('A'));
+	printf("%i\n", ft_isalnum('1'));
+	printf("%i\n", ft_isalnum('\n'));
+	printf("%i\n", ft_isalnum('V'));
+}
