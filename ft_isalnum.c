88ac6275688002916ac9b434779bdbfd3d072721
Retr02k <pedro210332@gmail.com>
2025-09-14 18:02:09 +0100

    deleted main on ft_isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,12 +6,11 @@
 /*   By: psilva-p <psilva-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 17:41:23 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/09/14 18:50:09 by psilva-p         ###   ########.fr       */
+/*   Updated: 2025/09/14 18:01:43 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_isalnum(char c)
 {
@@ -19,13 +18,4 @@ int	ft_isalnum(char c)
 		return (1);
 	else
 		return (0);
-}
-
-int main()
-{
-	printf("%i\n", ft_isalnum('a'));
-	printf("%i\n", ft_isalnum('A'));
-	printf("%i\n", ft_isalnum('1'));
-	printf("%i\n", ft_isalnum('\n'));
-	printf("%i\n", ft_isalnum('V'));
 }
