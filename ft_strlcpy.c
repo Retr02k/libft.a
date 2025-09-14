dc5a3ae5d0202ef4b012a142a9a49a7aeb8519cf
Retr02k <pedro210332@gmail.com>
2025-09-14 23:45:42 +0100

    ft_strlcpy.c && added to libft.h

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,32 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psilva-p <psilva-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/14 17:42:16 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/09/14 22:46:26 by psilva-p         ###   ########.fr       */
+/*   Created: 2025/09/14 21:42:49 by psilva-p          #+#    #+#             */
+/*   Updated: 2025/09/14 22:46:11 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
+#include "libft.h"
 
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-size_t strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlen(const	char	*str);
-int strncmp(const char s1, const char s2, size_t n);
-int	tolower(int	c);
-int	toupper(int	c);
+size_t strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t i;
 
-
-
-#endif
+	if (size > 0)
+	{
+		i = 0;
+		while (src[i] && (i < size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
