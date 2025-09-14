63d35b2e22a14374a6fe8f0f4240781a7c5f2132
Retr02k <pedro210332@gmail.com>
2025-09-14 20:56:16 +0100

    ft_tolower.c && added ft_toupper.c  and ft_tolower.c to libft.h

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psilva-p <psilva-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/14 17:42:16 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/09/14 19:56:36 by psilva-p         ###   ########.fr       */
+/*   Created: 2025/09/14 20:53:13 by psilva-p          #+#    #+#             */
+/*   Updated: 2025/09/14 20:54:35 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-size_t	ft_strlen(const	char	*str);
-int	tolower(int	c);
-int	toupper(int	c);
-
-
-
-#endif
+int	tolower(int	c)
+{
+	if (c >= 'a' && c <= 'Z')
+		c += 32;
+	return (c);
+}
