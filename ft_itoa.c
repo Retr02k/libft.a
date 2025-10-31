2280d68659832b8640d9c6feb979f98e78023a70
Retr02k <pedro210332@gmail.com>
2025-10-31 18:07:47 +0000

    ft_itoa.c protected

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: psilva-p <psilva-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 21:49:11 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/10/28 18:47:02 by psilva-p         ###   ########.fr       */
+/*   Updated: 2025/10/31 18:03:50 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,11 @@ static	int	int_count(int n)
 	int	count;
 
 	count = 0;
-	if (n < 0)
-	{
-		n *= -1;
-		while (n > 0)
-		{
-			n /= 10;
-			count++;
-		}
-		return (count);
-	}
-	else if (n == 0)
+	if (n == 0)
 		return (1);
-	while (n > 0)
+	if (n < 0)
+		count = 1;
+	while (n != 0)
 	{
 		n /= 10;
 		count++;
@@ -45,39 +37,47 @@ static char	*handle_zero(void)
 	if (!str)
 		return (NULL);
 	str[0] = '0';
-	str[1] = '\0';
 	return (str);
 }
 
-static	void	fill_string(char *str, int n, int i)
+static	void	fill_string(char *str, int n, int i, int is_negative)
 {
-	while (n > 0)
+	if (is_negative)
 	{
-		str[i] = (n % 10) + '0';
-		n /= 10;
-		i--;
+		while (n != 0)
+		{
+			str[i] = -(n % 10) + '0';
+			n /= 10;
+			i--;
+		}
+	}
+	else
+	{
+		while (n > 0)
+		{
+			str[i] = (n % 10) + '0';
+			n /= 10;
+			i--;
+		}
 	}
 }
 
 char	*ft_itoa(int n)
 {
 	int		size;
-	char	*str;
 	int		is_negative;
+	char	*str;
 
 	if (n == 0)
 		return (handle_zero());
 	is_negative = (n < 0);
-	size = int_count(n) + is_negative + 1;
+	size = int_count(n) + 1;
 	str = (char *)ft_calloc(size, sizeof(char));
 	if (!str)
 		return (NULL);
 	if (is_negative)
-	{
 		str[0] = '-';
-		n *= -1;
-	}
-	fill_string(str, n, size - 2);
+	fill_string(str, n, size - 2, is_negative);
 	return (str);
 }
 /* 
