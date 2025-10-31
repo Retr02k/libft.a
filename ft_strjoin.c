5cfa018d12c56b0c6ab20f14ef2fdfaf4050d4e9
Retr02k <pedro210332@gmail.com>
2025-10-31 17:48:23 +0000

    ft_strjoin.c protected

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: psilva-p <psilva-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 14:41:33 by psilva-p          #+#    #+#             */
-/*   Updated: 2025/10/31 15:52:28 by psilva-p         ###   ########.fr       */
+/*   Updated: 2025/10/31 17:40:05 by psilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,23 @@
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*str;
-	size_t	lens[3];
+	size_t	len1;
+	size_t	len2;
+	size_t	total_len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	lens[0] = ft_strlen(s1);
-	lens[1] = ft_strlen(s2);
-	lens[2] = lens[0] + lens[1];
-	str = (char *)ft_calloc(lens[2] + 1, sizeof(char));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (len1 > SIZE_MAX - len2)
+		return (NULL);
+	total_len = len1 + len2;
+	str = (char *)ft_calloc(total_len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_memcpy(str, (void *)s1, lens[0]);
-	return ((void)ft_memcpy(str + lens[0], (void *)s2, lens[1]), str);
+	ft_memcpy(str, (void *)s1, len1);
+	ft_memcpy(str + len1, (void *)s2, len2);
+	return (str);
 }
 
 // int main()
